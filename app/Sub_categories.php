26deaee80f254f29f1d4b09a74be83cef3f3901c
hsnbd <hasanbd666@gmail.com>
2017-10-24 00:54:16 +0600

    add profile

--- app/Sub_categories.php
+++ app/Sub_categories.php
@@ -6,12 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sub_categories extends Model
 {
-    public function categories()
-    {
-        return $this->belongsTo(Categories::class);
-    }
-    public function medicines()
-    {
-        return $this->belongsTo(Medicines::class);
-    }
+
 }
--- app/categories.php
+++ app/categories.php
@@ -6,8 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    public function subCategories()
-    {
-        return $this->hasMany(Sub_categories::class);
-    }
+
 }
