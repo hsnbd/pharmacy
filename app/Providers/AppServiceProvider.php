0695f204b88ccbd515d130370c1b667799e4b9c2
hsnbd <hasanbd666@gmail.com>
2017-10-25 01:54:42 +0600

    new update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,6 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment() == 'local') {
             URL::forceScheme('https');
         }
-        $cat = Categories::with('subCategories')->get();
-        View::share("cat", $cat);
     }
     /**
      * Register any application services.
