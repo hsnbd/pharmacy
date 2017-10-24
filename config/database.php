28e0e8ee38ad52644246e109116698b9705fb0fc
hsnbd <hasanbd666@gmail.com>
2017-10-25 02:16:41 +0600

    new update

--- config/database.php
+++ config/database.php
@@ -1,26 +1,47 @@
 <?php
 
-return [
+if ($this->app->environment() == 'local') {
+    return [
+        'default' => 'mysql',
 
+        'connections' => [
 
-
-    'default' => 'pgsql',
-
-    'connections' => [
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'mysql' => [
+                'driver' => 'mysql',
+                'host' => 'localhost',
+                'database' => 'php108',
+                'username' => 'root',
+                'password' => '',
+                'unix_socket' => env('DB_SOCKET', ''),
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ],
         ],
+
+        'migrations' => 'migrations',
+
+        'redis' => [
+
+            'client' => 'predis',
+
+            'default' => [
+                'host' => env('REDIS_HOST', '127.0.0.1'),
+                'password' => env('REDIS_PASSWORD', null),
+                'port' => env('REDIS_PORT', 6379),
+                'database' => 0,
+            ],
+
+        ],
+
+    ];
+}
+
+return [
+    'default' => 'pgsql',
+    'connections' => [
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => parse_url(getenv("DATABASE_URL"))["host"],
@@ -33,20 +54,14 @@ return [
             'sslmode' => 'prefer',
         ],
     ],
-
     'migrations' => 'migrations',
-
     'redis' => [
-
         'client' => 'predis',
-
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
-
     ],
-
 ];
